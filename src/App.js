8c76547e278609d9767be3900987e9ab7ea10c02
theghostyousee <patrickserise174@gmail.com>
2023-06-18 19:51:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [title, setTitle] = useState('Privacy');
+
+  useEffect(() => {
+    const words = ['Bingo', '0xBingo', 'BingAIo', 'Bing@!X', 'BinLAXSg'];
+    let currentIndex = 0;
+
+    const updateTitle = () => {
+      setTitle(words[currentIndex]);
+      currentIndex = (currentIndex + 1) % words.length;
+
+      const duration = currentIndex === 0 ? 2000 : 400; // Adjust durations as needed
+
+      setTimeout(updateTitle, duration);
+    };
+
+    updateTitle();
+
+    return () => clearTimeout(updateTitle);
+  }, []);
+
+  const handleButtonClick = () => {
+    window.location.href = 'https://twitter.com/intent/tweet?text=I%27m%20requesting%20Beta%20Access%20for%20%400xBingoProtocol';
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing">
+
+      <div className='title'>
+        <h1>{title}</h1>
+        <h1>Matters</h1>
+      </div>
+
+      <div className='paragraph'>
+        <p>0xBingo: AI prediction powerhouse for ERC20 tokens.</p>
+
+        <button  onClick={handleButtonClick}>Proof of Bingo</button>
+      </div>
+      
     </div>
   );
 }
